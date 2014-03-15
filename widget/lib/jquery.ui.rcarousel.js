fc5a906440c5db45bb47a679e29049e80ce58442
Wojciech Ryrych <wojtek@ryrych.pl>
2014-03-15 10:44:15 +0100

    fix problem with encoding fixes #52

--- widget/lib/jquery.ui.rcarousel.js
+++ widget/lib/jquery.ui.rcarousel.js
@@ -17,21 +17,21 @@
 
 			// create wrapper inside root element; this is needed for animating
 			$root
-				.addClass( "ui-carousel" )
+				.addClass( "ui-carousel" )
 				.children()
-				.wrapAll( "<div class='wrapper'></div>" );
-			
+				.wrapAll( "<div class='wrapper'></div>" );
+
 			// save all children of root element in ‘paths’ array
 			this._saveElements();
 
 			// make pages using paginate algorithm
-			this._generatePages();		
-			
+			this._generatePages();
+
 			this._loadElements();
-				
+
 			this._setCarouselWidth();
 			this._setCarouselHeight();
-			
+
 			// handle default event handlers
 			$( options.navigation.next ).click(
 				function( event ) {
@@ -39,17 +39,17 @@
 					event.preventDefault();
 				}
 			);
-			
+
 			$( options.navigation.prev ).click(
 				function( event ) {
 					_self.prev();
 					event.preventDefault();
 				}
-			);			
-			
+			);
+
 			data.navigation.next = $( options.navigation.next );
 			data.navigation.prev = $( options.navigation.prev );
-			
+
 			// stop on hover feature
 			$root.hover(
 				function() {
@@ -65,27 +65,27 @@
 					}
 				}
 			);
-			
+
 			this._setStep();
-			
+
 			// if auto mode is enabled run it
 			if ( options.auto.enabled ) {
 				this._autoMode( options.auto.direction );
 			}
-			
+
 			// broadcast event
 			this._trigger( "start" );
 		},
-		
-		_addElement: function( jQueryElement, direction ) {
+
+		_addElement: function( jQueryElement, direction ) {
 			var $root = $( this.element ),
-				$content = $root.find( "div.wrapper" ),
+				$content = $root.find( "div.wrapper" ),
 				options = this.options;
 
 			jQueryElement
 				.width( options.width )
 				.height( options.height );
-				
+
 			if ( options.orientation === "horizontal" ) {
 				$( jQueryElement ).css( "marginRight", options.margin );
 			} else {
@@ -94,37 +94,37 @@
 					"float": "none"
 				});
 			}
-			
+
 			if ( direction === "prev" ) {
-				
+
 				// clone event handlers and data as well
 				$content.prepend( jQueryElement.clone(true, true) );
 			} else {
 				$content.append( jQueryElement.clone(true, true) );
-			}			
+			}
 		},
-		
+
 		append: function( jqElements ) {
 			var $root = $( this.element ),
-				data = $root.data( "data" );
-				
+				data = $root.data( "data" );
+
 			// add new elements
 			jqElements.each(
-				function( i, el ) {
+				function( i, el ) {
 					data.paths.push( $(el) );
 				}
 			);
-			
+
 			data.oldPage = data.pages[data.oldPageIndex].slice(0);
 			data.appended = true;
-			
+
 			// rebuild pages
 			this._generatePages();
 		},
-		
+
 		_autoMode: function( direction ) {
 			var options = this.options,
-				data = $( this.element ).data( "data" );
+				data = $( this.element ).data( "data" );
 
 			if ( direction === "next" ) {
 				data.interval = setTimeout( $.proxy(this.next, this), options.auto.interval );
@@ -132,12 +132,12 @@
 				data.interval = setTimeout( $.proxy(this.prev, this), options.auto.interval );
 			}
 		},
-		
+
 		_checkOptionsValidity: function( options ) {
 			var i,
 				self = this,
 				_correctSteps = "";
-			
+
 			// for every element in options object check its validity
 			$.each(options,
 				function( key, value ) {
@@ -149,7 +149,7 @@
 								throw new Error( "visible should be defined as a positive integer" );
 							}
 							break;
-	
+
 						case "step":
 							if ( !value || typeof value !== "number" || value <= 0 || (Math.ceil(value) - value > 0) ) {
 								throw new Error( "step should be defined as a positive integer" );
@@ -160,64 +160,64 @@
 								for ( i = 1; i <= Math.floor(options.visible); i++ ) {
 									_correctSteps += ( i < Math.floor(value) ) ? i + ", " : i;
 								}
-								
+
 								throw new Error( "Only following step values are correct: " + _correctSteps );
 							}
 							break;
-	
+
 						case "width":
 							// width & height is defined by default so you can omit them to some extent
 							if ( !value || typeof value !== "number" || value <= 0 || Math.ceil(value) - value > 0 ) {
 								throw new Error( "width should be defined as a positive integer" );
 							}
 							break;
-		
+
 						case "height":
 							if ( !value || typeof value !== "number" || value <= 0 || Math.ceil(value) - value > 0 ) {
 								throw new Error("height should be defined as a positive integer");
 							}
 							break;
-		
+
 						case "speed":
 							if ( !value && value !== 0 ) {
 								throw new Error("speed should be defined as a number or a string");
 							}
-		
+
 							if ( typeof value === "number" && value < 0 ) {
 								throw new Error( "speed should be a positive number" );
 							} else if ( typeof value === "string" && !(value === "slow" || value === "normal" || value === "fast") ) {
 								throw new Error( 'Only "slow", "normal" and "fast" values are valid' );
 							}
 							break;
-		
+
 						case "navigation":
 							if ( !value || $.isPlainObject(value) === false ) {
 								throw new Error( "navigation should be defined as an object with at least one of the properties: 'prev' or 'next' in it");
 							}
-		
+
 							if ( value.prev && typeof value.prev !== "string" ) {
 								throw new Error( "navigation.prev should be defined as a string and point to '.class' or '#id' of an element" );
 							}
-		
+
 							if ( value.next && typeof value.next !== "string" ) {
 								throw new Error(" navigation.next should be defined as a string and point to '.class' or '#id' of an element" );
 							}
 							break;
-		
+
 						case "auto":
 							if ( typeof value.direction !== "string" ) {
 								throw new Error( "direction should be defined as a string" );
 							}
-		
+
 							if ( !(value.direction === "next" || value.direction === "prev") ) {
 								throw new Error( "direction: only 'right' and 'left' values are valid" );
 							}
-		
+
 							if ( isNaN(value.interval) || typeof value.interval !== "number" || value.interval < 0 || Math.ceil(value.interval) - value.interval > 0 ) {
 								throw new Error( "interval should be a positive number" );
 							}
 							break;
-		
+
 						case "margin":
 							if ( isNaN(value) || typeof value !== "number" || value < 0 || Math.ceil(value) - value > 0 ) {
 								throw new Error( "margin should be a positive number" );
@@ -227,7 +227,7 @@
 				}
 			);
 		},
-		
+
 		_createDataObject: function() {
 			var $root = $( this.element );
 
@@ -246,14 +246,14 @@
 				}
 			);
 		},
-		
+
 		_generatePages: function() {
 			var self = this,
 				options = this.options,
 				data = $( this.element ).data( "data" ),
 				_visible = options.visible,
 				_pathsLen = data.paths.length;
-				
+
 			// having 10 elements: A, B, C, D, E, F, G, H, I, J the algorithm
 			// creates 3 pages for ‘visible: 5’ and ‘step: 4’:
 			// [ABCDE], [EFGHI], [FGHIJ]
@@ -269,11 +269,11 @@
 				for ( var i = _pathsLen - 1; i >= _pathsLen - _visible; i-- ) {
 					data.lastPage.unshift( data.paths[i] );
 				}
-				
+
 				// and first page
 				for ( var i = 0; i < _visible; i++ ) {
 					data.pages[0][data.pages[0].length] = data.paths[i];
-				}				
+				}
 			}
 
 			function _islastPage( page ) {
@@ -287,7 +287,7 @@
 						break;
 					}
 				}
-				
+
 				return _isLast;
 			}
 
@@ -321,8 +321,8 @@
 					if ( _end > _pathsLen ) {
 						_end = _pathsLen;
 					}
-					
-					// when we run ouf of elements (_end - _start < _visible) we must add the difference at the begining
+
+					// when we run ouf of elements (_end - _start < _visible) we must add the difference at the begining
 					// in our example the 3rd page is [FGHIJ] and J element is added in the second step
 					// first we add [FGHI] as old elements
 					// we must assure that we have always ‘visible’ (5 in our example) elements
@@ -333,22 +333,22 @@
 					}
 
 					if ( _complement ) {
-						
+
 						// first add old elemets; for 3rd page it adds [FGHI…]
 						// remember the page we add to (_index)
 						_oldFirstEl = _start - ( _visible - (_end - _start) );
 						_oldLastEl = _oldFirstEl + ( _visible - (_end - _start) );
 						_index = _append( _oldFirstEl, _oldLastEl );
-						
+
 						// then add new elements; for 3th page it is J element:
 						// [fghiJ]
 						_append( _start, _end, _index );
 
 					} else {
-						
+
 						// normal pages like [ABCDE], [EFGHI]
 						_append( _start, _end );
-						
+
 						// next step
 						_start += options.step;
 					}
@@ -359,17 +359,17 @@
 			_init();
 			_paginate();
 		},
-		
+
 		getCurrentPage: function() {
 			var data = $( this.element ).data( "data" );
 			return data.pageIndex + 1;
 		},
-		
+
 		getTotalPages: function() {
 			var data = $( this.element ).data( "data" );
 			return data.pages.length;
 		},
-		
+
 		goToPage: function( page ) {
 			var	_by,
 				data = $( this.element ).data( "data" );
@@ -382,21 +382,21 @@
 				} else if ( page < 0 ) {
 					page = 0;
 				}
-				
+
 				data.pageIndex = page;
 				_by = page - data.oldPageIndex;
-				
+
 				if ( _by >= 0 ) {
 					//move by n elements from current index
 					this._goToNextPage( _by );
 				} else {
 					this._goToPrevPage( _by );
 				}
-				
+
 				data.oldPageIndex = page;
 			}
 		},
-		
+
 		_loadElements: function(elements, direction) {
 			var options = this.options,
 				data = $( this.element ).data( "data" ),
@@ -415,7 +415,7 @@
 				}
 			}
 		},
-		
+
 		_goToPrevPage: function( by ) {
 			var _page, _oldPage, _dist, _index, _animOpts, $lastEl, _unique, _pos, _theSame,
 				$root = $( this.element ),
@@ -424,13 +424,13 @@
 				data = $( this.element ).data( "data" );
 
 			// pick pages
-			if ( data.appended ) {
+			if ( data.appended ) {
 				_oldPage = data.oldPage;
-			} else {				
+			} else {
 				_oldPage = data.pages[data.oldPageIndex];
 			}
-			
-			_index = data.oldPageIndex + by;			
+
+			_index = data.oldPageIndex + by;
 			_page = data.pages[_index].slice( 0 );
 
 			// For example, the first time widget was initiated there were 5
@@ -442,26 +442,26 @@
 			// we compare the same
 			// pages, that is, the 2nd page from 5 elements and the 2nd from
 			// 10 elements. Thus what we do next is to decrement the index and
-			// loads the first page from 10 elements.			
+			// loads the first page from 10 elements.
 			$( _page ).each(
-				function( i, el ) {
-					if ( el.get(0) === $(_oldPage[i]).get(0) ) {
+				function( i, el ) {
+					if ( el.get(0) === $(_oldPage[i]).get(0) ) {
 						_theSame = true;
 					} else {
 						_theSame = false;
 					}
 				}
 			);
-			
-			if ( data.appended && _theSame ) {
-				if ( data.pageIndex === 0 ) {
+
+			if ( data.appended && _theSame ) {
+				if ( data.pageIndex === 0 ) {
 					_index = data.pageIndex = data.pages.length - 1;
 				} else {
 					_index = --data.pageIndex;
 				}
-				
+
 				_page = data.pages[_index].slice( 0 );
-			}			
+			}
 
 			// check if last element from _page appears in _oldPage
 			// for [ABCDFGHIJ] elements there are 3 pages for ‘visible’ = 6 and
@@ -487,7 +487,7 @@
 					}
 					--_pos;
 				}
-			}			
+			}
 
 			// load new elements
 			self._loadElements( _page, "prev" );
@@ -511,35 +511,35 @@
 					if ( !data.hoveredOver && options.auto.enabled ) {
 						// if autoMode is on and you change page manually
 						clearInterval( data.interval );
-						
+
 						self._autoMode( options.auto.direction );
 					}
 
 					// scrolling is finished, send an event
 					self._trigger("pageLoaded", null, {page: _index});
 				});
-				
+
 			// reset to deafult
-			data.appended = false;				
+			data.appended = false;
 		},
-		
+
 		_goToNextPage: function( by ) {
 			var _page, _oldPage, _dist, _index, _animOpts, $firstEl, _unique, _pos, _theSame,
 				$root = $( this.element ),
 				options = this.options,
-				data = $root.data( "data" ),				
+				data = $root.data( "data" ),
 				self = this;
 
 			// pick pages
-			if ( data.appended ) {
+			if ( data.appended ) {
 				_oldPage = data.oldPage;
-			} else {				
+			} else {
 				_oldPage = data.pages[data.oldPageIndex];
 			}
-			
-			_index = data.oldPageIndex + by;			
+
+			_index = data.oldPageIndex + by;
 			_page = data.pages[_index].slice( 0 );
-			
+
 			// For example, the first time widget was initiated there were 5
 			// elements: A, B, C, D, E and 2 pages for visible 4 and step 3:
 			// ABCD and BCDE. Then a user loaded next 5 elements so there were
@@ -551,16 +551,16 @@
 			// 10 elements. Thus what we do next is to increment the index and
 			// loads the second page from 10 elements.
 			$( _page ).each(
-				function( i, el ) {
-					if ( el.get(0) === $(_oldPage[i]).get(0) ) {
+				function( i, el ) {
+					if ( el.get(0) === $(_oldPage[i]).get(0) ) {
 						_theSame = true;
 					} else {
 						_theSame = false;
 					}
 				}
 			);
-	
-			if ( data.appended && _theSame ) {
+
+			if ( data.appended && _theSame ) {
 				_page = data.pages[++data.pageIndex].slice( 0 );
 			}
 
@@ -588,35 +588,35 @@
 					++_pos;
 				}
 			}
-			
-			// load new elements			
+
+			// load new elements
 			this._loadElements( _page, "next" );
 
 			// calculate the distance
 			_dist = options.width * _page.length + ( options.margin * _page.length );
-			
-			if ( options.orientation === "horizontal" ) {
+
+			if ( options.orientation === "horizontal" ) {
 				_animOpts = {scrollLeft: "+=" + _dist};
 			} else {
 				_animOpts = {scrollTop: "+=" + _dist};
 			}
-			
+
 			$root
 				.animate(_animOpts, options.speed, function() {
 					self._removeOldElements( "first", _page.length );
-					
+
 					if ( options.orientation === "horizontal" ) {
 						$root.scrollLeft( 0 );
 					} else {
 						$root.scrollTop( 0 );
 					}
-					
+
 					data.animated = false;
 
 					if ( !data.hoveredOver && options.auto.enabled ) {
 						// if autoMode is on and you change page manually
 						clearInterval( data.interval );
-						
+
 						self._autoMode( options.auto.direction );
 					}
 
@@ -624,22 +624,22 @@
 					self._trigger( "pageLoaded", null, {page: _index});
 
 			});
-				
+
 			// reset to deafult
 			data.appended = false;
 		},
-		
+
 		next: function() {
 			var	options = this.options,
 				data = $( this.element ).data( "data" );
 
 			if ( !data.animated ) {
 				data.animated = true;
-				
-				if ( !data.appended  ) {
+
+				if ( !data.appended  ) {
 					++data.pageIndex;
-				}				
-				
+				}
+
 				if ( data.pageIndex > data.pages.length - 1 ) {
 					data.pageIndex = 0;
 				}
@@ -649,7 +649,7 @@
 				data.oldPageIndex = data.pageIndex;
 			}
 		},
-		
+
 		prev: function() {
 			var	options = this.options,
 				data = $( this.element ).data( "data" );
@@ -657,10 +657,10 @@
 			if ( !data.animated ) {
 				data.animated = true;
 
-				if ( !data.appended ) {
+				if ( !data.appended ) {
 					--data.pageIndex;
 				}
-				
+
 				if ( data.pageIndex < 0 ) {
 					data.pageIndex = data.pages.length - 1;
 				}
@@ -670,7 +670,7 @@
 				data.oldPageIndex = data.pageIndex;
 			}
 		},
-		
+
 		_removeOldElements: function(position, length) {
 			// remove 'step' elements
 			var	$root = $( this.element );
@@ -678,37 +678,37 @@
 			for ( var i = 0; i < length; i++ ) {
 				if ( position === "first" ) {
 					$root
-						.find( "div.wrapper" )
+						.find( "div.wrapper" )
 							.children()
 							.first()
 							.remove();
 				} else {
 					$root
-						.find( "div.wrapper" )
+						.find( "div.wrapper" )
 							.children()
 							.last()
 							.remove();
 				}
 			}
 		},
-		
-		_saveElements: function() {
+
+		_saveElements: function() {
 			var $el,
 				$root = $( this.element ),
-				$elements = $root.find( "div.wrapper" ).children(),
+				$elements = $root.find( "div.wrapper" ).children(),
 				data = $root.data( "data" );
-				
+
 			$elements.each(
 				function( i, el ) {
 					$el = $( el );
-					
+
 					// keep element’s data and events
 					data.paths.push( $el.clone(true, true) );
 					$el.remove();
 				}
-			);		
+			);
 		},
-		
+
 		_setOption: function( key, value ) {
 			var _newOptions,
 				options = this.options,
@@ -720,11 +720,11 @@
 					options.speed = value;
 					$.Widget.prototype._setOption.apply( this, arguments );
 					break;
-	
+
 				case "auto":
 					_newOptions = $.extend( options.auto, value );
 					this._checkOptionsValidity({auto: _newOptions});
-	
+
 					if ( options.auto.enabled ) {
 						this._autoMode( options.auto.direction );
 					}
@@ -742,11 +742,11 @@
 			options.step = _step;
 			data.step = options.width * _step;
 		},
-		
+
 		_setCarouselHeight: function() {
 			var _newHeight,
 				$root = $( this.element ),
-				data = $( this.element ).data( "data" ),			
+				data = $( this.element ).data( "data" ),
 				options = this.options;
 
 			if ( options.orientation === "vertical" ) {
@@ -757,7 +757,7 @@
 
 			$root.height(_newHeight);
 		},
-		
+
 		_setCarouselWidth: function() {
 			var _newWidth,
 				$root = $( this.element ),
@@ -776,7 +776,7 @@
 				overflow: "hidden"
 			});
 		},
-		
+
 		options: {
 			visible: 3,
 			step: 3,
